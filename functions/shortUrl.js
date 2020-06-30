40d3b6696c4482cee5934a480a5d881682c94cce
Arpit Bhalla <arpitbhalla2001@gmail.com>
2020-06-30 22:14:52 +0530

    Improvements

--- functions/shortUrl.js
+++ functions/shortUrl.js
@@ -1,24 +1,17 @@
-const express = require('express');
-const router = express.Router()
-const sendRes = require('../util/respose')
 const db = require('../models/url')
 const { v1 } = require('uuid')
 
 const UID = (max = 6) => v1().replace(/-/g, "").substr(0, max)
 
-router.post("/", (req, res, next) => {
-    const { headers } = req
-    const API = headers['x-api-key']
-    if (!API)
-        return sendRes(res, "API Required")
-    const { url, max, custom } = req.body
+exports.handler = (event,context,sendRes) => {
+    const { url, max, custom } = event.body || {}
     let shortLink = custom || UID(max || 6)
     db.find({ short: shortLink }, (err, doc) => {
         let message = null;
         if (max < 5)
             message = "Link can not less than 5 characters"
         if (err)
-            return sendRes(res, err)
+            return sendRes(err,{})
         if (doc.length)
             shortLink = UID(max || 6), message = "Custom Link was not available"
 
@@ -27,16 +20,14 @@ router.post("/", (req, res, next) => {
             short: shortLink,
             createdAt: new Date().getTime()
         }).then(doc => {
-            sendRes(res, null, {
+            sendRes(null, {
                 shorted: doc.short,
                 message
             })
         }).catch(err => {
-            sendRes(res, err)
+            sendRes(err,{})
         })
     })
 
     return 0;
-})
-
-module.exports = router
+}
